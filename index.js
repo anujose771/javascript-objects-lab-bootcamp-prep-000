22b1c398790887a272d6a109870d4e1796482e8e
blue shadow <anujose1989@gmail.com>
2017-10-06 18:03:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {};
 
 var obj = { prop: 1 };
-function updateObjectWithKeyAndValue(obj, key, value){
-  object[key] = value;
-  return obj;
-}
-updateObjectWithKeyAndValue(obj, 'prop2', 2);
+// function updateObjectWithKeyAndValue(obj, key, value){
+//   object[key] = value;
+//   return obj;
+// }
+// updateObjectWithKeyAndValue(obj, 'prop2', 2);
 
 function updateObjectWithKeyAndValue(obj, key, value){
    return Object.assign({}, obj, { [key]: value })
